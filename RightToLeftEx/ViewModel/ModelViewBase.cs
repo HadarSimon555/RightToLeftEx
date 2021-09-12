d17085143317e6f4ee440c5710c9dec0b8487ec5
User <User@LAB2-2>
2021-09-12 09:29:25 +0300

    Add project files.

---
+++ RightToLeftEx/ViewModel/ModelViewBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace RightToLeftEx.ViewModel
+{
+    class ModelViewBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
